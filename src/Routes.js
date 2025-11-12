9a32c666352227bebf2ac404d2048772f997dfa2
smokusjorkus <johannelumauag11@gmail.com>
2025-11-12 11:38:40 +0800

    fixed conflict

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,12 @@ import RegisterPage from "./pages/Register/RegisterPage";
 import CleanerLoginPage from "./pages/CleanerLoginPage/CleanerLoginPage";
 import CustomerLoginPage from "./pages/CustomerLoginPage/CustomerLoginPage";
 import ChooseYourRole from "./pages/ChooseYourRole/ChooseYourRole";
+import CustomerHomePage from "./pages/CustomerHomePage/CustomerHomePage";
+import CustomerBookingPage from "./pages/CustomerBookingPage/CustomerBookingPage";
+import CustomerBookingSummary from "./pages/CustomerBookingSummary/CustomerBookingSummary";
+import CustomerPaymentPage from "./pages/CustomerPaymentPage/CustomerPaymentPage";
+import CustomerProfilePage from "./pages/CustomerProfilePage/CustomerProfilePage";
+import CustomerSettingsPage from "./pages/CustomerSettingsPage/CustomerSettingsPage";
 
 const AppRoutes = () => {
   return (
@@ -13,8 +19,17 @@ const AppRoutes = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/Register" element={<RegisterPage />} />
         <Route path="/login/cleaner" element={<CleanerLoginPage />} />
-        <Route path="/login/Customer" element={<CustomerLoginPage />} />
+        <Route path="/login/customer" element={<CustomerLoginPage />} />
         <Route path="/login/ChooseYourRole" element={<ChooseYourRole />} />
+        <Route path="/customer/home" element={<CustomerHomePage />} />
+        <Route path="/customer/booking" element={<CustomerBookingPage />} />
+        <Route
+          path="/customer/bookingSummary"
+          element={<CustomerBookingSummary />}
+        />
+        <Route path="/customer/payment" element={<CustomerPaymentPage />} />
+        <Route path="/customer/profile" element={<CustomerProfilePage />} />
+        <Route path="/customer/settings" element={<CustomerSettingsPage />} />
       </Routes>
     </Router>
   );
